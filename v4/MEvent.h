339ab84d634894dcbaa313b8c7fa7771cbe89ada
silveirasamuel <47599233+silveirasamuel@users.noreply.github.com>
2019-02-21 19:20:05 -0300

    Add files via upload

---
+++ v4/MEvent.h
@@ -0,0 +1,12 @@
+#ifndef M_EVENT_H
+#define M_EVENT_H
+
+class MEvent{
+	public:
+		int id;
+		int nParticles;
+	/*public:
+		void setId(Int_t id);
+		void setNParticles(Int_t nParticles);*/
+};
+#endif
